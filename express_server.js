48f0d9b4cfbba0bb9a427f4d96bb6dbf9dbaf937
mzparulina <pauline.revilla@gmail.com>
2023-01-10 05:01:55 -0500

    Add a new url view and route

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -22,9 +24,19 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.post("/urls", (req, res) => {
+  const newUrls = { id: generateRandomString(), longURL: req.body.longURL };
+  // console.log(newUrls); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -35,3 +47,8 @@ app.get("/urls/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2, 8);
+  // return Array.from(Array(6), () => Math.floor(Math.random() * 36).toString(36)).join('');
+};

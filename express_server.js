71d97b06a7fc20e114e0eb94df51e186d4190530
mzparulina <pauline.revilla@gmail.com>
2023-01-12 17:44:35 -0500

    Add registration page

--- express_server.js
+++ express_server.js
@@ -13,20 +13,9 @@ const urlDatabase = {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
+//==============================
+// POST METHODS
+//==============================
 app.post('/login', (req, res) => {
   const username = req.body.username;
   res.cookie('username', username);
@@ -54,6 +43,16 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/register", (req, res) => {
+  console.log('register');
+});
+//==============================
+// GET METHODS
+//==============================
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
 app.get("/urls", (req, res) => {
   const username = req.cookies.username;
   const templateVars = {
@@ -61,6 +60,23 @@ app.get("/urls", (req, res) => {
     username: username,
   };
   res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  const user = req.username;
+  const templateVars = {
+    username: user,
+  };
+  res.render('urls_register', templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.get("/urls/new", (req, res) => {
